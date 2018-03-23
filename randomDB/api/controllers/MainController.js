fd1765b54df3da8e74b499dceff97e216ffb69fa
Mark Strelecky <streleck@gmail.com>
2018-03-23 14:27:40 -0500

    view, delete working. edit...

--- randomDB/api/controllers/MainController.js
+++ randomDB/api/controllers/MainController.js
@@ -41,8 +41,52 @@ module.exports = {
         })
         //res.redirect('/main');
     },
-    getdata:function(req,res){
-        res.send({noise:"looloo"});
+    getdata:function(req, res){
+        Main.find({}).exec(function(err,data){
+            if(err){
+                res.send(500, {error: "Database Error"});
+            }
+            res.send({data});
+        });
+        
+    },
+    delete:function(req,res){
+        Main.destroy({id:req.params.id}).exec(function(err){
+            if(err){
+                res.send(500, {error: "Database Error"});
+            }
+            res.redirect("/main/view");
+
+            return false;
+        })
+    },
+    edit:function(req,res){
+        Main.findOne({id:req.params.id}).exec(function(err, entry){
+            if(err){
+                res.send(500, {error: "Database Error"});
+            }
+            var dataSet = [];
+            for(field in entry.document){
+                var newField = [];
+                newField.push(field);
+                newField.push(entry.document[field]);
+                dataSet.push(newField);
+            }
+            res.view("edit", {id:req.params.id,entry:dataSet});
+
+            return false;
+        })
+    },
+    update:function(req,res){
+            
+        var data = req.body;
+        //data.id = req.params;
+        Main.update({id: req.params}, {data}).exec(function(err){
+            if(err){
+                res.send(500, {error: "Database Error"});
+            }
+            res.redirect("/main/view");
+        });
     }
 };
 
